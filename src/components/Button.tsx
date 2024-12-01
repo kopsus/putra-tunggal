a603c9286a2637f9b03364456d1a2554447e26e6
kopsus <emonsetio01@gmail.com>
2024-12-01 19:46:42 +0700

    create button components

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface IButton {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const ButtonLarge = ({ children, className }: IButton) => {
+  return (
+    <button className={`${className} font-bold shadow py-[18px] px-16`}>
+      {children}
+    </button>
+  );
+};
+
+const ButtonMedium = ({ children, className }: IButton) => {
+  return (
+    <button className={`${className} font-bold shadow py-3 px-12`}>
+      {children}
+    </button>
+  );
+};
+
+const ButtonSmall = ({ children, className }: IButton) => {
+  return (
+    <button className={`${className} font-bold shadow py-2 px-8`}>
+      {children}
+    </button>
+  );
+};
+
+export { ButtonLarge, ButtonMedium, ButtonSmall };
---
+++ src/libs/fonts.ts
@@ -0,0 +1,7 @@
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400"],
+});
+
+export { poppins };
