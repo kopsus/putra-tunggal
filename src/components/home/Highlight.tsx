675052dbb6cbb70bde1db72d8351b80cb40eb4e1
kopsus <emonsetio01@gmail.com>
2024-12-09 14:59:22 +0700

    fixing highlight home page

--- src/components/home/Highlight.tsx
+++ src/components/home/Highlight.tsx
@@ -13,7 +13,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 export const Highlight = () => {
   return (
     <div className="Container">
-      <div className="md:h-[415px] rounded-3xl bg-white shadow-md border relative">
+      <div className="h-[50vh] md:h-[415px] rounded-3xl bg-white shadow-md border relative">
         <Swiper
           pagination={true}
           navigation={true}
@@ -27,18 +27,20 @@ export const Highlight = () => {
           {dataBanner.map((item, index) => (
             <SwiperSlide key={index}>
               <div className="relative grid md:grid-cols-2 h-full p-10 lg:px-16">
-                <div className="h-full flex flex-col gap-4 text-center md:text-start justify-between md:justify-center items-center md:items-start z-20">
+                <div className="h-full flex flex-col text-center md:text-start justify-between md:justify-center items-center md:items-start z-20">
                   <div className="flex flex-col gap-4">
                     <p className="titleHighlight md:leading-[45px] text-primary">
                       {item.title}
                     </p>
-                    <p className="text-primary/70 leading-6">{item.desc}</p>
+                    <p className="text-xs md:text-sm text-primary/70 leading-6">
+                      {item.desc}
+                    </p>
                   </div>
                   <ButtonMedium className="bg-red text-white">
                     BUAT JANJI SEKARANG
                   </ButtonMedium>
                 </div>
-                <div></div>
+                <div className="hidden md:block"></div>
                 <div className="absolute bottom-0 mx-auto w-full flex justify-center md:justify-end lg:right-20">
                   <Image
                     src={highlightIMG}
