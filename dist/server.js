6a58564cb165795bf5bd8a2da0a58e728c3a44f4
kopsus <emonsetio01@gmail.com>
2025-01-08 22:46:53 +0700

    tesst

--- dist/server.js
+++ dist/server.js
@@ -8,32 +8,31 @@ const port = 3000;
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 app.prepare().then(() => {
-    const httpServer = createServer(handler);
-    const io = new Server(httpServer);
-    io.on("connection", (socket) => {
-        console.log('a user connected');
-        socket.on('join_room', (roomId) => {
-            socket.join(roomId);
-            console.log(`user join room ${roomId}`);
-        });
-        socket.on('send_message', async (data) => {
-            try {
-                io.to(data.roomId).emit('receive_message', data);
-            }
-            catch (error) {
-                console.log(error);
-            }
-        });
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
+  const httpServer = createServer(handler);
+  const io = new Server(httpServer);
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("join_room", (roomId) => {
+      socket.join(roomId);
+      console.log(`user join room ${roomId}`);
     });
-    httpServer
-        .once("error", (err) => {
-        console.error(err);
-        process.exit(1);
+    socket.on("send_message", async (data) => {
+      try {
+        io.to(data.roomId).emit("receive_message", data);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  });
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
     })
-        .listen(port, () => {
-        console.log(`> Ready on http://${hostname}:${port}`);
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
     });
 });
